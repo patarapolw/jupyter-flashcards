1af7833fd79d2cfbd06825a356558e12a2a7bcc2
patarapolw <patarapolw@gmail.com>
2018-07-18 20:26:33 +0700

    pyexcel-handsontable now works offline.

--- jupyter_flashcards/dir.py
+++ jupyter_flashcards/dir.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+import inspect
 
-TRUE_ROOT = Path(__file__).parent.parent
+MODULE_ROOT = Path(inspect.getframeinfo(inspect.currentframe()).filename).parent
+TRUE_ROOT = MODULE_ROOT.parent
 
 
 def module_path(filename):
-    return str(Path(__file__).parent.joinpath(filename).relative_to(TRUE_ROOT))
+    return str(MODULE_ROOT.joinpath(filename).relative_to(TRUE_ROOT))
